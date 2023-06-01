a90f110ff86af222c35bc12fd21f36023b755868
MeCap02 <mateocostilla123@gmail.com>
2023-06-01 19:53:18 -0300

    rama 2

---
+++ Empleado.cs
@@ -0,0 +1,32 @@
+/* 1. Crear una clase para almacenar la siguiente información: una clase Empleado con
+los siguientes atributos:
+a. Nombre (string), Apellido (string), Fecha de nacimiento (datetime),
+Estado civil (char), Género (char), fecha de ingreso en la empresa
+(datetime), sueldo Básico (double), Cargo (cargos)
+b. La enumeración cargos con los siguientes elementos: Auxiliar;
+Administrativo; Ingeniero; */
+namespace Empleados;
+
+public class Empleado
+{
+    private 
+
+   public string nombre;
+   public string apellido;
+   public DateTime fechaNac;
+   public char estadoCivil;
+   public char genero;
+   public DateTime fechaIngreso;
+   public double sueldo;
+   public cargos cargo;
+   private enum cargos
+   {
+        Auxiliar = 1,
+        Administrativo = 2,
+        Ingeniero = 3
+   }
+
+    public Edad(DateTime fechaNac, DateTime Now)
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,61 +1,12 @@
-﻿using EspacioCalculadora;
-Console.WriteLine("Hello, World!");
+﻿using Empleados;
 
-Calculadora cal = new Calculadora();
+Empleado nuevo = new Empleado();
 
-double valor=0;
-int opcion;
-
-    Console.WriteLine("Menu Principal");
-    Console.WriteLine("1. Sumar");
-    Console.WriteLine("2. Restar");
-    Console.WriteLine("3. Multiplicar");
-    Console.WriteLine("4. Dividir");
-    Console.WriteLine("5. Limpiar");
-    Console.WriteLine("6. Salir");
-    Console.WriteLine("Ingrese la opcion: ");
-
-    int.TryParse(Console.ReadLine(), out opcion);
-
-while(opcion!=6){
-        Console.WriteLine();
-        switch(opcion){
-            case 1:
-                Console.WriteLine("Ingrese el valor que desea sumar: ");
-                double.TryParse(Console.ReadLine(),out valor); 
-                cal.Suma(valor);
-                break;    
-            case 2: 
-                Console.WriteLine("Ingrese el valor que desea restar: ");
-                double.TryParse(Console.ReadLine(),out valor); 
-                cal.Resta(valor);
-                break;
-            case 3: 
-                Console.WriteLine("Ingrese el valor que desea multiplicar: ");
-                double.TryParse(Console.ReadLine(),out valor); 
-                cal.Multiplicar(valor);
-                break;
-            case 4: 
-                Console.WriteLine("Ingrese el valor que desea dividir: ");
-                double.TryParse(Console.ReadLine(),out valor); 
-                cal.Dividir(valor);
-                break;    
-            case 5: 
-                cal.Limpiar();
-                break;    
-            case 6: 
-                break;    
-        }
-        Console.WriteLine(cal.Resultado);
-        Console.WriteLine();
-        Console.WriteLine("Menu Principal");
-        Console.WriteLine("1. Sumar");
-        Console.WriteLine("2. Restar");
-        Console.WriteLine("3. Multiplicar");
-        Console.WriteLine("4. Dividir");
-        Console.WriteLine("5. Limpiar");
-        Console.WriteLine("6. Salir");
-        Console.WriteLine("Ingrese la opcion: ");
-
-        int.TryParse(Console.ReadLine(), out opcion);
-}
+nuevo.nombre=Leandro;
+nuevo.apellido=Cordoba;
+nuevo.fechaIngreso=10/01/2023;
+nuevo.fechaNac=01/03/1999;
+nuevo.estadoCivil=S;
+nuevo.sueldo=5000;
+nuevo.genero=M;
+nuevo.cargo=Auxiliar;
