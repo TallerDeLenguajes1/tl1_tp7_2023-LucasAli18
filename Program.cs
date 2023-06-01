eafbbf414dcb1f7430b1fc36ade40bcbf0f0d25b
MeCap02 <mateocostilla123@gmail.com>
2023-06-01 19:31:24 -0300

    Calculadora 2.0

--- Program.cs
+++ Program.cs
@@ -17,34 +17,37 @@ int opcion;
 
     int.TryParse(Console.ReadLine(), out opcion);
 
-while(opcion!=0){
+while(opcion!=6){
         Console.WriteLine();
-        Console.WriteLine("Ingrese el valor con el que desea trabajar: ");
-        double.TryParse(Console.ReadLine(),out valor);
         switch(opcion){
-            case 1: 
+            case 1:
+                Console.WriteLine("Ingrese el valor que desea sumar: ");
+                double.TryParse(Console.ReadLine(),out valor); 
                 cal.Suma(valor);
-                Console.WriteLine(cal.Resultado);
                 break;    
             case 2: 
+                Console.WriteLine("Ingrese el valor que desea restar: ");
+                double.TryParse(Console.ReadLine(),out valor); 
                 cal.Resta(valor);
-                Console.WriteLine(cal.Resultado);
                 break;
             case 3: 
+                Console.WriteLine("Ingrese el valor que desea multiplicar: ");
+                double.TryParse(Console.ReadLine(),out valor); 
                 cal.Multiplicar(valor);
-                Console.WriteLine(cal.Resultado);
                 break;
             case 4: 
+                Console.WriteLine("Ingrese el valor que desea dividir: ");
+                double.TryParse(Console.ReadLine(),out valor); 
                 cal.Dividir(valor);
-                Console.WriteLine(cal.Resultado);
                 break;    
             case 5: 
                 cal.Limpiar();
-                Console.WriteLine(cal.Resultado);
                 break;    
             case 6: 
                 break;    
         }
+        Console.WriteLine(cal.Resultado);
+        Console.WriteLine();
         Console.WriteLine("Menu Principal");
         Console.WriteLine("1. Sumar");
         Console.WriteLine("2. Restar");
